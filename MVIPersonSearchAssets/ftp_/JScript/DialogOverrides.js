8f9b9e5d36a16efa6109f65408e079c6faf14ece
Christopher Nichols <chnichol@microsoft.com>
2019-10-02 17:39:30 -0400

    Added VEIS.Workflows project

--- MVIPersonSearchAssets/ftp_/JScript/DialogOverrides.js
+++ MVIPersonSearchAssets/ftp_/JScript/DialogOverrides.js
@@ -5,18 +5,21 @@ function assignSelectedRecord(e) {
         height: 250,
         position: 1
     };
+
     var a = {};
     a["entity_name"] = "incident";
     a["selected_records_count"] = "1";
     a["entity_id"] = e;
     a["show_assign_to_me_option"] = !0;
+    
     Xrm.Navigation.openDialog("AssignQueue", i, a).then(function (e) {
         Xrm.Page.data.refresh().then(function () {
             Xrm.Page.ui.refreshRibbon();
         }).catch((err) => {
-            console.log(err);
+            alert(err);
         });
     }).catch((err) => {
-        console.log(err);
+
+        alert('err open: ' + err);
     });
 }
---
+++ VEIS.Workflows/GetQueueTeam.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VEIS.Workflows
+{
+    public class GetQueueTeam : CodeActivity
+    {
+        [Output("Team")]
+        [ReferenceTarget("team")]
+        public OutArgument<EntityReference> TeamOutArgument
+        {
+            get;
+            set;
+        }
+
+        [Input("Queue")]
+        [ReferenceTarget("queue")]
+        [RequiredArgument]
+        public InArgument<EntityReference> QueueInArgument
+        {
+            get;
+            set;
+        }
+
+        internal CodeActivityContext ActivityContext { get; private set; }
+        internal IWorkflowContext WorkflowExecutionContext { get; private set; }
+        internal IOrganizationServiceFactory ServiceFactory { get; set; }
+        internal IOrganizationService OrganizationService { get; set; }
+        internal ITracingService TracingService { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            this.ActivityContext = context;
+            // Obtain the execution context service from the service provider.
+            this.WorkflowExecutionContext = context.GetExtension<IWorkflowContext>();
+
+            ServiceFactory = context.GetExtension<IOrganizationServiceFactory>();
+
+            // Use the factory to generate the Organization Service.
+            this.OrganizationService = this.ServiceFactory.CreateOrganizationService(null);
+
+            var queueRef = QueueInArgument.Get<EntityReference>(context);
+
+            QueryExpression query = new QueryExpression("team");
+            query.Criteria = new FilterExpression();
+            query.Criteria.AddCondition("queueid", ConditionOperator.Equal, queueRef.Id);
+            query.TopCount = 1;
+
+            var entCol = this.OrganizationService.RetrieveMultiple(query);
+            if(entCol.Entities.Count == 1)
+            {
+                TeamOutArgument.Set(context, entCol.Entities.First().ToEntityReference());
+            }
+        }
+    }
+}
